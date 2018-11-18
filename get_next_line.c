28c6ce10b16fb067eff06a6e0fbd35614376a727
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-11-18 19:03:51 +0100

    add new version

--- get_next_line.c
+++ get_next_line.c
@@ -6,84 +6,70 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 22:47:06 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/17 20:03:32 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/18 19:03:10 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "stdio.h"	
 
-char    *ft_realloc(char *str, char *buf)
-{
-	int     j;
-	int     i;
-	char    *new;
-
-	i = ft_strlen(str);
-	j = ft_strlen(buf);
-	new = NULL;
-	if (!(new = (char*)malloc(sizeof(char) * (i + j + 1))))
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-	{
-		new[i] = str[i];
-		i++;
-	}
-	j = 0;
-	while (buf[j] != '\0')
-	{
-		new[i + j] = buf[j];
-		j++;
-	}
-	new[i + j] = '\0';
-	if (str != NULL)
-		free(str);
-	return (new);
-}
-
-int		get_next_malloc(char **line, char *str)
+int		ft_getline(char **line, char *str, char *over)
 {
 	int		i;
 
 	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
-	if(!(line[0] = ft_strnew(i)))
+	if(!(line[0] = ft_strsub(str, 0, i)))
 		return (-1);
-	ft_strncpy(line[0], str, i);
-	return (i);
+	ft_memmove(over, str + i + 1, ft_strlen(str) - i);
+	over[i] = '\0';
+	free(str);
+	return (0);
+}
+
+char	*str_realloc(char *str, char *buf)
+{
+	char	*tmp;
+
+	if(!(tmp = ft_strjoin(str, buf)))
+		return (NULL);
+	free(str);
+	return (tmp);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	int				ret;
 	char			buf[BUFF_SIZE + 1];
-	static char		*str;
-	int				sz_return;
+	char			*str;
+	static char		over[BUFF_SIZE + 1];
 
-	if (fd == -1 || !line)
+	if (!(str = ft_strnew(0)))
 		return (-1);
-	if (!str)
+	if (ft_strlen(over) > 0)
 	{
-		if (!(str = ft_strnew(0)))
-			return (-1);
+		str = ft_strjoin(str, over);
+		over[0] = '\0';
+		if (ft_strchr(str, '\n'))
+		{
+			if (ft_getline(line, str, over) == -1)
+				return (-1);
+			else
+				return (1);
+		}
 	}
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
-		if (ret == -1)
-			return (-1);
 		buf[ret] = '\0';
-		if(!(str = ft_realloc(str, buf)))
+		if(!(str = str_realloc(str, buf)))
 			return (-1);
+		if (ft_strchr(str, '\n'))
+			break;
 	}
-	if (ft_strlen(str) > 0)
-	{
-		if ((sz_return = get_next_malloc(line, str)) == -1)
-			return (-1);
-		ft_memmove(str, str + sz_return + 1, ft_strlen(str) - sz_return);
-		return (1);	
-	}
-	line[0] = NULL;
+	if (ft_getline(line, str, over) == -1)
+		return (-1);
+	else
+		return (1);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 15:46:24 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/17 18:33:41 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/18 19:00:49 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "libft.h"
 #include "stdlib.h"
 #include "unistd.h"
-#define BUFF_SIZE 1
+#define BUFF_SIZE 4
 
 int		get_next_line(const int fd, char **line);
 
