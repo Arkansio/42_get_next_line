2cb3b32fbf419ed849713f40c74b233ec6800fd5
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-11-18 21:50:02 +0100

    norme gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 22:47:06 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/18 21:15:45 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/18 21:48:22 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		ft_getline(char **line, char *str, char *over)
 		return (-1);
 	ft_memcpy(over, str + i + 1, ft_strlen(str + i));
 	free(str);
-	return (ft_strlen(str + i + 1));
+	return (1);
 }
 
 char	*str_realloc(char *str, char *buf)
@@ -37,16 +37,43 @@ char	*str_realloc(char *str, char *buf)
 	return (tmp);
 }
 
+char	*read_gnl(const int fd, char *str)
+{
+	char	buf[BUFF_SIZE + 1];
+	int		ret;
+
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		if (ret == -1)
+			return (NULL);
+		if (!(str = str_realloc(str, buf)))
+			return (NULL);
+		if (ft_strchr(str, '\n'))
+			break ;
+	}
+	if (ret == -1)
+		return (NULL);
+	return (str);
+}
+
+char	*ft_check(const int fd, char **line)
+{
+	char	*str;
+
+	if (fd < 0 || !line)
+		return (NULL);
+	if (!(str = ft_strnew(0)))
+		return (NULL);
+	return (str);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	int				ret;
-	char			buf[BUFF_SIZE + 1];
 	char			*str;
 	static char		over[BUFF_SIZE + 1];
-	
-	if (fd < 0 || !line)
-		return (-1);
-	if (!(str = ft_strnew(0)))
+
+	if (!(str = ft_check(fd, line)))
 		return (-1);
 	if (ft_strlen(over) > 0)
 	{
@@ -55,29 +82,17 @@ int		get_next_line(const int fd, char **line)
 		over[0] = '\0';
 		if (ft_strchr(str, '\n'))
 		{
-			if (!ft_getline(line, str, over))
+			if (ft_getline(line, str, over) == -1)
 				return (-1);
 			return (1);
 		}
 	}
-	while ((ret = read(fd, buf, BUFF_SIZE)))
-	{
-		buf[ret] = '\0';
-		if (ret == -1)
-			return (-1);
-		if (!(str = str_realloc(str, buf)))
-			return (-1);
-		if (ft_strchr(str, '\n'))
-			break ;
-	}
+	if (!(str = read_gnl(fd, str)))
+		return (-1);
 	if (ft_strlen(str) <= 0)
 	{
 		line[0] = NULL;
 		return (0);
 	}
-	if (ft_getline(line, str, over) == -1)
-		return (-1);
-	else
-		return (1);
-	return (0);
+	return (ft_getline(line, str, over));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 15:46:24 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/18 19:41:29 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/18 21:48:43 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "libft.h"
-#include "stdlib.h"
-#include "unistd.h"
-#define BUFF_SIZE 1024
+# include "libft.h"
+# include "stdlib.h"
+# include "unistd.h"
+# define BUFF_SIZE 1024
 
 int		get_next_line(const int fd, char **line);
 
