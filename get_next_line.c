fe5f7ba599757573b55883dd003901cdb6513a14
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-11-18 21:16:03 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 22:47:06 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/18 19:49:01 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/18 21:15:45 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "stdio.h"	
+#include "stdio.h"
 
 int		ft_getline(char **line, char *str, char *over)
 {
@@ -20,7 +20,7 @@ int		ft_getline(char **line, char *str, char *over)
 	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
-	if(!(line[0] = ft_strsub(str, 0, i)))
+	if (!(line[0] = ft_strsub(str, 0, i)))
 		return (-1);
 	ft_memcpy(over, str + i + 1, ft_strlen(str + i));
 	free(str);
@@ -31,7 +31,7 @@ char	*str_realloc(char *str, char *buf)
 {
 	char	*tmp;
 
-	if(!(tmp = ft_strjoin(str, buf)))
+	if (!(tmp = ft_strjoin(str, buf)))
 		return (NULL);
 	free(str);
 	return (tmp);
@@ -43,33 +43,38 @@ int		get_next_line(const int fd, char **line)
 	char			buf[BUFF_SIZE + 1];
 	char			*str;
 	static char		over[BUFF_SIZE + 1];
-
+	
 	if (fd < 0 || !line)
 		return (-1);
 	if (!(str = ft_strnew(0)))
 		return (-1);
 	if (ft_strlen(over) > 0)
 	{
-		str = str_realloc(str, over);
+		if (!(str = str_realloc(str, over)))
+			return (-1);
 		over[0] = '\0';
 		if (ft_strchr(str, '\n'))
 		{
-//			printf("value over: %s\n", over);
-			ft_getline(line, str, over);
+			if (!ft_getline(line, str, over))
+				return (-1);
 			return (1);
-//			printf("real Value %s: \n", over);
 		}
 	}
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		if(!(str = str_realloc(str, buf)))
+		if (ret == -1)
+			return (-1);
+		if (!(str = str_realloc(str, buf)))
 			return (-1);
 		if (ft_strchr(str, '\n'))
-			break;
+			break ;
 	}
 	if (ft_strlen(str) <= 0)
+	{
+		line[0] = NULL;
 		return (0);
+	}
 	if (ft_getline(line, str, over) == -1)
 		return (-1);
 	else
