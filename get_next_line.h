c52166b277a3123502d0f0dbf0d749773177e92f
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-11-19 17:00:12 +0100

    change header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 15:46:24 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/19 14:59:47 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/19 16:59:27 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 # define GET_NEXT_LINE_H
 
 # include "libft.h"
-# include "stdlib.h"
-# include "unistd.h"
+# include <stdlib.h>
+# include <unistd.h>
 # define BUFF_SIZE 2
 
 int		get_next_line(const int fd, char **line);
